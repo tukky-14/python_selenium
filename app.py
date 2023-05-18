3ade1776e672c0909f920b8bc8e9bd7408831213
tukky-14 <bigoonage0104@gmail.com>
2023-05-18 22:29:52 +0900

    検索結果を出力す流処理を追加

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 # Chromeドライバーを起動
@@ -9,14 +10,22 @@ driver = webdriver.Chrome()
 driver.get("http://www.google.com")
 
 # 検索ボックスに入力
-search_box = driver.find_element(by=webdriver.common.by.By.NAME, value="q")
+search_box = driver.find_element(by=By.NAME, value="q")
 search_box.send_keys("Hello, Selenium!")
 
 # 検索を実行
 search_box.send_keys(Keys.RETURN)
 
 # 結果が表示されるまで大輝
-time.sleep(5)
+time.sleep(1)
+
+# 検索結果のURLを取得
+search_results = driver.find_element(By.ID, "rso").find_elements(By.TAG_NAME, "a")
+for result in search_results:
+    title = result.find_element(By.TAG_NAME, "h3")
+    url = result.get_attribute('href')
+    print(title.text)
+    print(url)
 
 # ブラウザを閉じる
 driver.quit()
