e6ec9aefa07ff3e8a4a42148364369e1ab264cf6
tukky-14 <bigoonage0104@gmail.com>
2023-05-21 18:54:59 +0900

    取得したリンク情報を出力できるよう更新

--- app.py
+++ app.py
@@ -35,16 +35,21 @@ with open("screenshot.png", "wb") as file:
 
 # 検索結果のURLを取得
 search_results = driver.find_element(By.ID, "rso").find_elements(By.TAG_NAME, "a")
+search_links = []
 for result in search_results:
     try:
         title = result.find_element(By.CLASS_NAME, "LC20lb")
         url = result.get_attribute('href')
         if title.text != "" and url != "":
-            print(title.text)
-            print(url)
-            print()
+            search_links.append(title.text)
+            search_links.append(url)
+            search_links.append("")
     except NoSuchElementException:
         error = 1
 
 # ブラウザを閉じる
 driver.quit()
+
+with open('output.txt', 'w') as file:
+    output = '\n'.join(str(item) for item in search_links)
+    file.write(output)
